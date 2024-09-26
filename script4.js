f476e5925f3648394992b530c0a6ed0649729d62
RenathaLoiza <143304174+RenathaLoiza@users.noreply.github.com>
2024-09-26 16:55:19 -0300

    Add files via upload

---
+++ script4.js
@@ -0,0 +1,14 @@
+function dadoscadastro() {
+
+    alert("Olá, bem vindo!")
+
+    let nome = prompt("Digitar seu nome?");
+    let sobrenome= prompt ("Digitar seu sobrenome?");
+    let telefone=prompt("Digitar seu Telefone?");
+    let idade =prompt("Digitar seu idade?");
+    let email=prompt("Digitar seu email?");
+    let senha=prompt("Digitar seu senha?");
+
+   alert(" Seu nome é " + nome + " e " + sobrenome + " sua idade "+idade+ " telefone "+telefone + " seu email e senha é " + email+ " e "+senha+"." );
+
+}
